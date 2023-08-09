7245f8f47ba0786ec53e8f7d8e907678dfcc37b3
RickBm400 <rjbm29@gmail.com>
2023-08-08 22:48:39 -0500

    feat: project section folder view

--- src/components/Editor/ProjectSection.vue
+++ src/components/Editor/ProjectSection.vue
@@ -1,10 +1,36 @@
 <template>
-  <main>
-    <v-row class="mx-4 my-2" align="center">
-      <v-icon class="mr-1" size="14">mdi-archive</v-icon>
-      <span class="title-tab">Project</span>
-    </v-row>
-  </main>
+    <div class="mx-4 my-2">
+      <div class="d-flex justify-space-between">
+        <div>
+          <v-icon class="mr-1" size="14">mdi-archive</v-icon>
+          <span class="title-tab">Project</span>
+        </div>
+        <div>
+          <v-btn icon flat size="x-small" color="transparent">
+            <v-icon color="white">
+              mdi-dots-vertical
+            </v-icon>
+          </v-btn>
+          <v-btn icon flat size="x-small" color="transparent">
+            <v-icon color="white">
+              mdi-plus
+            </v-icon>
+          </v-btn>
+        </div>
+      </div>
+
+      <div class="three">
+        <ul>
+          <li class="folder"><v-icon size="14"> mdi-folder </v-icon> Proyecto
+            <ul>
+              <li>ola</li>
+            </ul>
+          </li>
+          <li class="folder"> <v-icon size="14"> mdi-folder </v-icon> Proyecto2 </li>
+          <li class="folder"> <v-icon size="14"> mdi-folder </v-icon> Proyecto3 </li>
+        </ul>
+      </div>
+      </div>
 </template>
 
 <script>
@@ -21,5 +47,57 @@ export default {
   font-size: 14px !important;
   letter-spacing: 0 !important;
   text-transform: none !important;
+}
+.three{
+  font-family: 'Roboto Mono', monospace;
+  font-size: 11px;
+}
+
+li {
+  margin-left: 20px;
+  position: relative;
+  color: white;
+}
+
+.folder {
+  cursor: pointer;
+}
+
+.folder:hover { 
+  color: #55a192 ;
+}
+
+li::before{
+  position: absolute;
+  top: 15px;
+  left: -15px;
+  width: 10px;
+  height: 1px;
+  margin: auto;
+  content: '';
+  background-color: #506d7f;
+}
+
+li::after {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: -15px;
+  width: 1px;
+  height: 100%;
+  content: '';
+  background-color: #506d7f;
+}
+
+li:last-child:after {
+  height: 15px;
+}
+
+ul{
+  list-style: none;
+}
+
+.document{
+  color: green;
 }
 </style>
