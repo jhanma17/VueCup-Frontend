233d8328084e7387c77fc16ab3f52ea5b1d19fb3
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2023-08-24 12:48:50 -0500

    Create LinkTemplate.vue

---
+++ src/components/ComponentTemplates/BasicComponents/LinkTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <a class="no-selection">
+    {{ props.text }}
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "LinkTemplate",
+  props: {
+    props: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style>
+.no-selection {
+  user-select: none;
+}
+</style>
