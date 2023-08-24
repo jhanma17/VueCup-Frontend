1998231cdadf0a63ded8efbccb845d14a7b46ed8
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2023-08-24 12:48:55 -0500

    Create TitleTemplate.vue

---
+++ src/components/ComponentTemplates/BasicComponents/TitleTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="no-selection">
+    {{ props.text }}
+    <slot></slot>
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "TitleTemplate",
+  props: {
+    props: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style>
+.no-selection {
+  user-select: none;
+}
+</style>
