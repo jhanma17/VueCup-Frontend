cc8850c014a7bc17362fe0f953c28823f31c9596
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2023-08-24 12:48:47 -0500

    Create BodyTemplate.vue

---
+++ src/components/ComponentTemplates/BasicComponents/BodyTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <span class="no-selection">
+    {{ props.text }}
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "BodyTemplate",
+  props: {
+    props: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style>
+.no-selection {
+  user-select: none;
+}
+</style>
