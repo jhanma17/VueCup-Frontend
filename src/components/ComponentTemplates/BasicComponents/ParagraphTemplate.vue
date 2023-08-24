4c988418fc4a2d9f714b0cbdad80c4ea085419f6
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2023-08-24 12:48:53 -0500

    Create ParagraphTemplate.vue

---
+++ src/components/ComponentTemplates/BasicComponents/ParagraphTemplate.vue
@@ -0,0 +1,24 @@
+<template>
+  <p class="no-selection">
+    {{ props.text }}
+    <slot></slot>
+  </p>
+</template>
+
+<script>
+export default {
+  name: "ParagraphTemplate",
+  props: {
+    props: {
+      type: Object,
+      required: false,
+    },
+  },
+};
+</script>
+
+<style>
+.no-selection {
+  user-select: none;
+}
+</style>
