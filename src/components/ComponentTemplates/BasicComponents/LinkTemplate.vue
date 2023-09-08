5dbcc50813694fb7dff61752cb0803301c88a792
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2023-09-08 15:15:37 -0500

    Feat: Link props

--- src/components/ComponentTemplates/BasicComponents/LinkTemplate.vue
+++ src/components/ComponentTemplates/BasicComponents/LinkTemplate.vue
@@ -12,6 +12,40 @@ export default {
     props: {
       type: Object,
       required: false,
+      default: () => {
+        return {
+          text: "Link",
+          fontFamily: "Roboto",
+          fontStyle: "Regular",
+          size: 16,
+          color: "#000000",
+        };
+      },
+    },
+  },
+  computed: {
+    style() {
+      return {
+        fontFamily: this.props.fontFamily,
+        fontSize: this.props.size + "px",
+        color: this.props.color,
+        ...this.formattedFontStyle,
+      };
+    },
+    formattedFontStyle() {
+      //separate the font style into fontStyle and fontWeight
+      let fontStyle = this.props.fontStyle || "normal";
+      let fontWeight = "normal";
+      if (fontStyle.includes("Bold")) {
+        fontWeight = "bold";
+      }
+      if (fontStyle.includes("Italic")) {
+        fontStyle = "italic";
+      }
+      return {
+        fontStyle: fontStyle,
+        fontWeight: fontWeight,
+      };
     },
   },
 };
