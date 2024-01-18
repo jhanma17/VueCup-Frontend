9bdd2d7f888cc45e45ff78f1e8d14e2394fa69dc
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2024-01-18 16:50:55 -0500

    Feat: Project Tree basis

--- src/components/Editor/ProjectSection/ProjectTree/ProjectNode.vue
+++ src/components/Editor/ProjectSection/ProjectTree/ProjectNode.vue
@@ -1,31 +1,58 @@
 <template>
-  <draggable
-    class="project-node"
-    :element="element"
-    :options="{group:'project-tree'}"
-    :move="move"
-    @end="end"
+  <v-col cols="12" class="py-1 pr-0" :class="isSelected ? 'selected-bg' : ''">
+    <v-icon density="compact" @click="toggleExpand()" v-if="component.children">
+      {{ expanded ? "mdi-chevron-down" : "mdi-chevron-right" }}
+    </v-icon>
+    <v-icon density="compact" v-else> </v-icon>
+    {{ component.type }}
+  </v-col>
+  <v-col
+    v-if="component.children && expanded"
+    v-for="child in component.children"
+    :key="child.id"
+    cols="12"
+    class="py-1 pr-0"
   >
-  </draggable>
+    <ProjectNode :component="child" />
+  </v-col>
 </template>
 
 <script>
-import draggable from 'vuedraggable';
+import { mapState } from "pinia";
+import { componentsStore } from "@/stores/components";
 
 export default {
   name: "ProjectNode",
-  components: {
-    draggable
-  },
   props: {
-    element: {
+    component: {
       type: Object,
       required: true,
     },
   },
-}
+  data() {
+    return {
+      expanded: false,
+    };
+  },
+  computed: {
+    ...mapState(componentsStore, ["inspectedComponent"]),
+    isSelected() {
+      return (
+        this.inspectedComponent &&
+        this.inspectedComponent.id == this.component.id
+      );
+    },
+  },
+  methods: {
+    toggleExpand() {
+      this.expanded = !this.expanded;
+    },
+  },
+};
 </script>
 
 <style>
-
+.selected-bg {
+  background-color: rgba(245, 245, 245, .3);
+}
 </style>
