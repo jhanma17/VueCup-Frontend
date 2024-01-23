ecac5af5b546b9d833d281a8f75cb415c0d7bf27
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2024-01-23 11:45:54 -0500

    Feat: select from project

--- src/components/Editor/ProjectSection/ProjectTree/ProjectNode.vue
+++ src/components/Editor/ProjectSection/ProjectTree/ProjectNode.vue
@@ -1,10 +1,21 @@
 <template>
-  <v-col cols="12" class="py-1 pr-0" :class="isSelected ? 'selected-bg' : ''">
-    <v-icon density="compact" @click="toggleExpand()" v-if="component.children">
+  <v-col
+    cols="12"
+    class="py-1 pr-0 clickable"
+    :class="isSelected ? 'selected-bg' : ''"
+    @click="inspectComponent(component)"
+  >
+    <v-icon
+      density="compact"
+      @click.stop="toggleExpand()"
+      v-if="component.children"
+    >
       {{ expanded ? "mdi-chevron-down" : "mdi-chevron-right" }}
     </v-icon>
     <v-icon density="compact" v-else> </v-icon>
-    {{ component.type }}
+    <span>
+      {{ component.type }}
+    </span>
   </v-col>
   <v-col
     v-if="component.children && expanded"
@@ -18,7 +29,7 @@
 </template>
 
 <script>
-import { mapState } from "pinia";
+import { mapState, mapActions } from "pinia";
 import { componentsStore } from "@/stores/components";
 
 export default {
@@ -44,6 +55,7 @@ export default {
     },
   },
   methods: {
+    ...mapActions(componentsStore, ["inspectComponent"]),
     toggleExpand() {
       this.expanded = !this.expanded;
     },
@@ -53,6 +65,9 @@ export default {
 
 <style>
 .selected-bg {
-  background-color: rgba(245, 245, 245, .3);
+  background-color: rgba(245, 245, 245, 0.3);
+}
+.clickable {
+  cursor: pointer;
 }
 </style>
