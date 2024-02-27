58e98486dbf758d56b7884b072db9b32affa4b6f
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2024-02-27 14:37:49 -0500

    Feat: Create ordered List template

---
+++ src/components/ComponentTemplates/BasicComponents/ListComponents/OrderedListTemplate.vue
@@ -0,0 +1,33 @@
+<template>
+  <ol class="no-selection" :style="style">
+    <slot></slot>
+  </ol>
+</template>
+
+<script>
+export default {
+  name: "OrderedListTemplate",
+  props: {
+    props: {
+      type: Object,
+      required: false,
+      default: () => {
+        return {
+        };
+      },
+    },
+  },
+  computed: {
+    style() {
+      return {
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.no-selection {
+  user-select: none;
+}
+</style>
