3de399c17a0919d437aac84ddb4b850e11e0b32e
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2024-03-03 00:44:18 -0500

    Feat: padding and margin added to components style

--- src/components/ComponentTemplates/BasicComponents/TextComponents/BodyTemplate.vue
+++ src/components/ComponentTemplates/BasicComponents/TextComponents/BodyTemplate.vue
@@ -43,6 +43,14 @@ export default {
       return {
         fontStyle: fontStyle,
         fontWeight: fontWeight,
+        marginTop: this.props.marginTop + "px",
+        marginBottom: this.props.marginBottom + "px",
+        marginRight: this.props.marginRight + "px",
+        marginLeft: this.props.marginLeft + "px",
+        paddingTop: this.props.paddingTop + "px",
+        paddingBottom: this.props.paddingBottom + "px",
+        paddingRight: this.props.paddingRight + "px",
+        paddingLeft: this.props.paddingLeft + "px",
       };
     },
   },
