6798d6733c365069fe7aafd3c1313211575a2138
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2024-04-01 17:03:51 -0500

    Feat: home as test page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,581 @@
 <template>
-  <main>
-  </main>
-</template>
-
-<script setup>
-</script>
+  <div class="Background2">
+    <div class="Ticket3">
+      <div class="Row14">
+        <div class="ArtistNameContainer13">
+          <span class="ArtistName14"> Artist Name </span>
+        </div>
+        <div class="RecordNameContainer8">
+          <span class="RecordName12"> Record Name </span>
+        </div>
+        <div class="TicketInfoContainer7">
+          <div class="Column115">
+            <span class="Body19"> Modern Polymath </span>
+            <span class="Body20"> Crosstown Studios </span>
+          </div>
+          <div class="Column217">
+            <span class="Body21"> Music & Arts </span>
+            <span class="Body22"> Liverpool </span>
+          </div>
+          <div class="Column316">
+            <span class="Body23"> MMXXIII </span>
+          </div>
+        </div>
+      </div>
+      <div class="QrInfo5">
+        <div class="QrCodeContainer9">
+          <img
+            src="https://cdn-icons-png.freepik.com/512/60/60902.png"
+            class="QrCodeImage25"
+          />
+        </div>
+        <div class="IdCodeContainer24">
+          <span class="IdCode26"> MPSN01 </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template> 
+  <style>
+.Background2 {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: #b6b6b6;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: #020202af;
+  border-style: none;
+  border-radius: 0px;
+  border-top: 0px none #020202af;
+  border-bottom: 0px none #020202af;
+  border-left: 0px none #020202af;
+  border-right: 0px none #020202af;
+}
+.Ticket3 {
+  width: 60%;
+  height: 30%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 0px solid #000000af;
+  border-bottom: 0px solid #000000af;
+  border-left: 0px solid #000000af;
+  border-right: 0px solid #000000af;
+}
+.Row14 {
+  width: 80%;
+  height: 100%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 0px solid #000000af;
+  border-bottom: 0px solid #000000af;
+  border-left: 0px solid #000000af;
+  border-right: 0px solid #000000af;
+}
+.ArtistNameContainer13 {
+  width: 100%;
+  height: 40%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 2px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 2px solid #000000af;
+  border-bottom: 2px solid #000000af;
+  border-left: 2px solid #000000af;
+  border-right: 2px solid #000000af;
+}
+.ArtistName14 {
+  font-family: Roboto;
+  font-size: 70px;
+  color: #000000;
+  font-style: Bold;
+  font-weight: bold;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 30px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.RecordNameContainer8 {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 2px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 2px solid #000000af;
+  border-bottom: 2px solid #000000af;
+  border-left: 2px solid #000000af;
+  border-right: 2px solid #000000af;
+}
+.RecordName12 {
+  font-family: Open Sans;
+  font-size: 50px;
+  color: #000000;
+  font-style: Bold;
+  font-weight: bold;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 30px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.TicketInfoContainer7 {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 2px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 2px solid #000000af;
+  border-bottom: 2px solid #000000af;
+  border-left: 2px solid #000000af;
+  border-right: 2px solid #000000af;
+}
+.Column115 {
+  width: 40%;
+  height: 100%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 30px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: transparent;
+  border-style: none;
+  border-radius: 0px;
+  border-top: 0px none transparent;
+  border-bottom: 0px none transparent;
+  border-left: 0px none transparent;
+  border-right: 0px none transparent;
+}
+.Body19 {
+  font-family: Open Sans;
+  font-size: 25px;
+  color: #000000;
+  font-style: Regular;
+  font-weight: normal;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.Body20 {
+  font-family: Roboto;
+  font-size: 25px;
+  color: #000000;
+  font-style: Regular;
+  font-weight: normal;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.Column217 {
+  width: 40%;
+  height: 100%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: transparent;
+  border-style: none;
+  border-radius: 0px;
+  border-top: 0px none transparent;
+  border-bottom: 0px none transparent;
+  border-left: 0px none transparent;
+  border-right: 0px none transparent;
+}
+.Body21 {
+  font-family: Open Sans;
+  font-size: 25px;
+  color: #000000;
+  font-style: Regular;
+  font-weight: normal;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.Body22 {
+  font-family: Open Sans;
+  font-size: 25px;
+  color: #000000;
+  font-style: Regular;
+  font-weight: normal;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.Column316 {
+  width: 20%;
+  height: 100%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: flex-end;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 30px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: transparent;
+  border-style: none;
+  border-radius: 0px;
+  border-top: 0px none transparent;
+  border-bottom: 0px none transparent;
+  border-left: 0px none transparent;
+  border-right: 0px none transparent;
+}
+.Body23 {
+  font-family: Open Sans;
+  font-size: 25px;
+  color: #000000;
+  font-style: Regular;
+  font-weight: normal;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.QrInfo5 {
+  width: 20%;
+  height: 100%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 0px;
+  border-color: transparent;
+  border-style: none;
+  border-radius: 0px;
+  border-top: 0px none transparent;
+  border-bottom: 0px none transparent;
+  border-left: 0px none transparent;
+  border-right: 0px none transparent;
+}
+.QrCodeContainer9 {
+  width: 100%;
+  height: 70%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 2px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 2px solid #000000af;
+  border-bottom: 2px solid #000000af;
+  border-left: 2px solid #000000af;
+  border-right: 2px solid #000000af;
+}
+.QrCodeImage25 {
+  width: 80%;
+  height: 80%;
+  display: block;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex-grow: 0;
+  flex-shrink: 0;
+  object-fit: fill;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+.IdCodeContainer24 {
+  width: 100%;
+  height: 30%;
+  display: flex;
+  position: static;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 0;
+  flex-shrink: 0;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+  background-color: transparent;
+  background-image: url(none);
+  border-width: 2px;
+  border-color: #000000af;
+  border-style: solid;
+  border-radius: 0px;
+  border-top: 2px solid #000000af;
+  border-bottom: 2px solid #000000af;
+  border-left: 2px solid #000000af;
+  border-right: 2px solid #000000af;
+}
+.IdCode26 {
+  font-family: Roboto;
+  font-size: 40px;
+  color: #000000;
+  font-style: Bold;
+  font-weight: bold;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  padding-right: 0px;
+  padding-left: 0px;
+}
+</style> 
+  
