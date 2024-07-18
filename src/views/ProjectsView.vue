df661fd6383bfe8603d2c725b932dc8e3df34e0d
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2024-07-18 16:39:08 -0500

    Feat: Fetch projects

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -6,14 +6,16 @@
       </v-col>
 
       <v-col cols="auto">
-        <v-btn @click="handleNewProject()" color="grey--darken-3" dark> + New Project </v-btn>
+        <v-btn @click="handleNewProject()" color="grey--darken-3" dark>
+          + New Project
+        </v-btn>
       </v-col>
     </v-row>
 
     <v-divider></v-divider>
 
     <v-row class="mt-5 mx-5">
-      <v-col cols="3">
+      <v-col cols="3" v-if="showNewProject">
         <v-img
           class="mx-auto rounded-lg"
           cover
@@ -33,6 +35,28 @@
           @keyup.enter="createProject()"
         ></v-text-field>
       </v-col>
+
+      <v-col cols="3" v-for="project in projects" :key="project._id">
+        <v-img
+          class="mx-auto rounded-lg mb-3"
+          cover
+          width="auto"
+          :aspect-ratio="16 / 9"
+          :src="
+            project.preview && project.preview !== ''
+              ? project.preview
+              : 'https://www.publicdomainpictures.net/pictures/40000/nahled/gray-background-1361959709geQ.jpg'
+          "
+        >
+        </v-img>
+        <span class="text-white ml-1 text-subtitle-1">
+          {{ project.name }}
+        </span>
+        <br />
+        <span class="ml-1 text-subtitle-2">
+          {{ timeDifference(project.updatedAt) }}
+        </span>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -49,7 +73,7 @@ export default {
   },
   methods: {
     handleNewProject() {
-      this.showNewProject = !this.showNewProject;
+      this.showNewProject = true;
 
       this.$nextTick(() => {
         this.$refs["new-project-input"].focus();
@@ -57,16 +81,64 @@ export default {
       });
     },
     async createProject() {
-      const response = await this.axios({
-        method: "POST",
-        url: "/project/create",
-        data: {
-          name: this.newProjectName,
-        },
-      })
+      try {
+        const response = await this.axios({
+          method: "POST",
+          url: "/projects/create",
+          data: {
+            name: this.newProjectName,
+          },
+        });
 
-      console.log(response.data);
+        this.projects.push(response.data.project);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.showNewProject = false;
+        this.newProjectName = "New Project";
+      }
     },
+    async fetchProjects() {
+      try {
+        const response = await this.axios({
+          method: "GET",
+          url: "/projects",
+        });
+
+        this.projects = response.data.projects;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    timeDifference(updatedAt) {
+      const date = new Date(updatedAt);
+      const now = new Date();
+      const diff = now - date;
+      const seconds = Math.floor(diff / 1000);
+      const minutes = Math.floor(seconds / 60);
+      const hours = Math.floor(minutes / 60);
+      const days = Math.floor(hours / 24);
+      const months = Math.floor(days / 30);
+      const years = Math.floor(months / 12);
+      if (years > 0) {
+        return years + " years ago";
+      } else if (months > 0) {
+        return months + " months ago";
+      } else if (days > 0) {
+        return days + " days ago";
+      } else if (hours > 0) {
+        return hours + " hours ago";
+      } else if (minutes > 0) {
+        return minutes + " minutes ago";
+      } else if (seconds > 0) {
+        return seconds + " seconds ago";
+      } else {
+        return "Just now";
+      }
+    },
+  },
+  async created() {
+    await this.fetchProjects();
   },
 };
 </script>
